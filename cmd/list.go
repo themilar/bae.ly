4be5345cb2d7d6fbba00590aede3d1823a77d0ab
Oluwadamilare <joeydre69@gmail.com>
2024-04-05 16:07:18 +0100

    implement list cmd

--- cmd/list.go
+++ cmd/list.go
@@ -1,13 +1,14 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/themilar/bae.ly/bae"
 )
 
 // listCmd represents the list command
@@ -20,8 +21,19 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
+		var limit int
+		if len(args) < 1 {
+			limit = 5
+		}
+		limit, _ = strconv.Atoi(args[0])
+		if limit < 1 {
+			fmt.Print("limit must be a positive integer")
+			return
+		}
+		fmt.Println(bae.List(limit))
 	},
 }
 
